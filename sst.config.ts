1e8ffe5b6d0500829a03bf4a0099c265904cc149
Ru Chern Chong <hello@ruchern.xyz>
2023-10-13 17:36:43 +0800

    Add custom domain

--- sst.config.ts
+++ sst.config.ts
@@ -10,7 +10,9 @@ export default {
   },
   stacks(app) {
     app.stack(function Site({ stack }) {
-      const site = new NextjsSite(stack, "site");
+      const site = new NextjsSite(stack, "site", {
+        customDomain: "https://next-sst-example.ruchern.xyz",
+      });
 
       stack.addOutputs({
         SiteUrl: site.url,
