15c60447c13d41c7e66a5994a7443db8b2d8fd58
Ru Chern Chong <hello@ruchern.xyz>
2023-10-21 12:30:49 +0800

    Add Security Headers

---
+++ middleware.ts
@@ -0,0 +1,44 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+export const middleware = (request: NextRequest) => {
+  const nonce = Buffer.from(crypto.randomUUID()).toString("base64");
+  const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'nonce-${nonce} 'strict-dynamic';
+  style-src 'self' 'nonce-${nonce}';
+  img-src 'self' blob: data:;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  frame-ancestors 'none';
+  block-all-mixed-content;
+  upgrade-insecure-requests;
+`;
+
+  const requestHeaders = new Headers(request.headers);
+  requestHeaders.set("x-nonce", nonce);
+  requestHeaders.set("X-DNS-Prefetch-Control", "on");
+  requestHeaders.set(
+    "Strict-Transport-Security",
+    "max-age=63072000; includeSubDomains; preload",
+  );
+  requestHeaders.set("X-Frame-Options", "SAMEORIGIN");
+  requestHeaders.set(
+    "Permissions-Policy",
+    "camera=(), microphone=(), geolocation=(), browsing-topics=()",
+  );
+  requestHeaders.set("X-Content-Type-Options", "nosniff");
+  requestHeaders.set("Referrer-Policy", "origin-when-cross-origin");
+
+  requestHeaders.set(
+    "Content-Security-Policy",
+    cspHeader.replace(/\s{2,}/g, " ").trim(),
+  );
+
+  return NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
+};
